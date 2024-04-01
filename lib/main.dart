b51c2bf80b9d7097a6ded9f6d8c36dad2153b7c4
Limon49 <mrlimon28@gmail.com>
2024-04-01 11:45:22 +0600

    commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_list_issues/routes/route_generator.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sizer/sizer.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+//route observer
+final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async{
+  await dotenv.load(
+    fileName: '.env'
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (BuildContext context, Orientation orientation, DeviceType deviceType) {
+        return MaterialApp(
+          title: 'Flutter List Issues',
+          navigatorObservers: [routeObserver],
+          navigatorKey: navigatorKey,
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.generateRoute,
+          theme: ThemeData(
+            scaffoldBackgroundColor: CupertinoColors.systemGrey4,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+        );
+      },
+    );
+  }
+}
+
